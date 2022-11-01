73e792ba48d556e5593d4053c6b62c8acd5fbaf1
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-11-01 11:57:22 -0600

    add basic movement and end game detection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame, random, math, sys
 from itertools import repeat
-from pygame import mixer
+from pygame import K_SPACE, mixer
 import scripts
 
 pygame.init()
@@ -30,13 +30,14 @@ class Background(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("Assets/Art/background.png")
-        self.position = (WIDTH/2, HEIGHT/2)
+        self.position = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(-4, 0)
         self.rect = self.image.get_rect(center=self.position)
         self.id = "background"
 
-    # TODO Update this later to allow for scrolling background
     def update(self):
-        self.position = self.position 
+        self.position += self.vel
+        self.rect.center = self.position
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -45,29 +46,36 @@ class Player(pygame.sprite.Sprite):
         self.original_image = self.image
         self.position = vec(WIDTH/2-250, HEIGHT/2)
         self.rect = self.image.get_rect(center=self.position)
-        self.vel = vec(0, 0)
-        self.acceleration = vec(0, -0.2)
+        self.vel = vec(0, 4)
+        self.acceleration = vec(0, -24)
         self.id = "player"
 
     def reset(self):
         self.position = vec(WIDTH/2-250, HEIGHT/2)
         self.image = pygame.transform.rotate(self.original_image, 0)
         self.rect = self.image.get_rect(center=self.position)
-        self.vel = vec(0, 0)
-        self.acceleration = vec(0, -0.2)
+        self.vel = vec(0, 4)
+        self.acceleration = vec(0, -24)
 
     def update(self):
+        if self.vel.magnitude() > vec(0, 4).magnitude():
+            self.vel = vec(0, 4)
+        elif self.vel.magnitude() < vec(0, -24).magnitude():
+            self.vel = vec(0, -24)
+
+        self.vel += (0, 4)
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
             self.vel += self.acceleration
 
         self.position += self.vel
         self.rect.center = self.position
-        self.leave_screen()
+        self.did_leave_screen()
 
-    def leave_screen(self):
+    def did_leave_screen(self):
         if self.position.y >= HEIGHT:
-            pass # End the game.
+            return True
 
 class Pipe(pygame.sprite.Sprite):
     def __init__(self):
@@ -142,10 +150,58 @@ def game_loop(all_sprites, pipes, backgrounds, player, offset):
 
         fps_clock.tick(FPS)
         all_sprites.draw(screen)
+
+        if player.did_leave_screen():
+            offset = scripts.shake()
+            game_over(all_sprites, pipes, backgrounds, player, offset)
+
         org_screen.blit(screen, next(offset))
         pygame.display.update()
 
 def game_over(all_sprites, pipes, backgrounds, player, offset):
-    print("This is the game over screen.")
+    mixer.music.load("Assets/SFX/happy.mp3")
+    mixer.music.set_volume(0.5)
+    mixer.music.play(-1)
+    click = False
+
+    scripts.reset_game(all_sprites, pipes, backgrounds, player)
+
+    background = Background()
+    backgrounds.add(background)
+
+    while True:
+        screen.fill((0, 0, 0))
+        backgrounds.draw(screen)
+
+        scripts.draw_text("GAME OVER", title_font, (0, 0, 0), screen, WIDTH/2, HEIGHT/2-100)
+        mouseX, mouseY = pygame.mouse.get_pos()
+        play_button = pygame.Rect(WIDTH/2, HEIGHT/2, 200, 50)
+        play_button.center=(WIDTH/2, HEIGHT/2)
+        quit_button = pygame.Rect(WIDTH/2, HEIGHT/2+75, 200, 50)
+        quit_button.center=(WIDTH/2, HEIGHT/2+75)
+        if play_button.collidepoint((mouseX, mouseY)):
+            if click:
+                main_menu(all_sprites, pipes, backgrounds, player, offset)
+        if quit_button.collidepoint((mouseX, mouseY)):
+            if click:
+                pygame.quit()
+                sys.exit()
+        pygame.draw.rect(screen, (0, 0, 0), play_button)
+        scripts.draw_text("MAIN MENU", game_font, (255, 255, 255), screen, WIDTH/2, HEIGHT/2)
+        pygame.draw.rect(screen, (0, 0, 0), quit_button)
+        scripts.draw_text("QUIT", game_font, (255, 255, 255), screen, WIDTH/2, HEIGHT/2+75)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        fps_clock.tick(FPS)
+        org_screen.blit(screen, next(offset))
+        pygame.display.update()
 
 main_menu(all_sprites, pipes, backgrounds, player, offset)
