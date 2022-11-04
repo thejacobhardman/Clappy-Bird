744e0ba2293a04baec5b7d4874d36785a5f7fd62
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-11-04 11:46:47 -0600

    add load and animate sprite functions to scripts

--- scripts.py
+++ scripts.py
@@ -26,6 +26,29 @@ def check_collisions(sprite, group):
         return True
     return False
 
+def load_player_sprite():
+    player_sprite = [
+        pygame.image.load("Assets/Art/Bird Sprite/frame-1.png"),
+        pygame.image.load("Assets/Art/Bird Sprite/frame-2.png"),
+        pygame.image.load("Assets/Art/Bird Sprite/frame-3.png"),
+        pygame.image.load("Assets/Art/Bird Sprite/frame-4.png"),
+        pygame.image.load("Assets/Art/Bird Sprite/frame-5.png"),
+        pygame.image.load("Assets/Art/Bird Sprite/frame-6.png"),
+        pygame.image.load("Assets/Art/Bird Sprite/frame-7.png"),
+        pygame.image.load("Assets/Art/Bird Sprite/frame-8.png")
+    ]
+
+    for i in range(8):
+        player_sprite[i] = pygame.transform.scale(player_sprite[i], (100, 68))
+
+    return player_sprite
+
+def animate_sprite(frames, index):
+    if index == len(frames):
+        return frames[0]
+    else:
+        return frames[index+1]
+
 def reset_game(all_sprites, pipes, backgrounds, player):
     all_sprites.empty()
     pipes.empty()
