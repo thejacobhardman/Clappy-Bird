aef59acaf95236e67c93e0aedb82c87bf0f5e93d
connorswanson1 <connorswanson13@gmail.com>
2022-12-05 11:30:59 -0700

    formatting update

--- youtubeDl.py
+++ youtubeDl.py
@@ -3,19 +3,19 @@ import os
 
 
 def yt_download(video_url):
-    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
+    video_info = youtube_dl.YoutubeDL().extract_info(url=video_url, download=False)
     filename = f"{video_info['title']}"
     save_path = os.path.join(os.getcwd(), "CustomLevels", filename)
 
-    options={
+    options = {
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
-            
-         }], 
-         'outtmpl': save_path
+
+        }],
+        'outtmpl': save_path
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
