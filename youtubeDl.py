96bdf2cf7aaf3e88ed0c896fe92bbd64dd4bcbad
connorswanson1 <connorswanson13@gmail.com>
2022-12-05 00:42:53 -0700

    Save file to CustomLevels

--- youtubeDl.py
+++ youtubeDl.py
@@ -1,14 +1,11 @@
 import youtube_dl
-from os import listdir
-from os.path import isfile, join
 import os
 
 
 def yt_download(video_url):
     video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
-    #filename = f"{video_info['title']}.mp3"
-    #save_path = '/'.join(os.getcwd().split('/')[:3])
-    #save_path = listdir("CustomLevels")
+    filename = f"{video_info['title']}"
+    save_path = os.path.join(os.getcwd(), "CustomLevels", filename)
 
     options={
         'format': 'bestaudio/best',
@@ -18,10 +15,11 @@ def yt_download(video_url):
             'preferredquality': '192',
             
          }], 
-         'outtmpl': f"{video_info['title']}"
+         'outtmpl': save_path
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
 
     print("Download complete... {}".format({video_info['title']}))
+    print(save_path)
