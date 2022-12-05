0a6082366b2daac0e8608d27e464832ff871ed04
connorswanson1 <connorswanson13@gmail.com>
2022-12-04 23:10:59 -0700

    fix download process, got rid of filename variable

--- youtubeDl.py
+++ youtubeDl.py
@@ -1,15 +1,27 @@
 import youtube_dl
+from os import listdir
+from os.path import isfile, join
+import os
+
 
 def yt_download(video_url):
     video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
-    filename = f"{video_info['title']}.mp3"
+    #filename = f"{video_info['title']}.mp3"
+    #save_path = '/'.join(os.getcwd().split('/')[:3])
+    #save_path = listdir("CustomLevels")
+
     options={
-        'format':'bestaudio/best',
-        'keepvideo':False,
-        'outtmpl':filename,
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+            
+         }], 
+         'outtmpl': '/'.join(os.getcwd().split('/')[:3]) + {video_info['title']},  
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
 
-    print("Download complete... {}".format(filename))
+    print("Download complete... {}".format({video_info['title']}))
