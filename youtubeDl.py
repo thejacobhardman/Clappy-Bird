69638e7b57a5adfabe3276a4f84f7629fc1b85c1
connorswanson1 <connorswanson13@gmail.com>
2022-12-05 17:09:07 -0700

    fix downloader to coles version

--- youtubeDl.py
+++ youtubeDl.py
@@ -3,19 +3,17 @@ import os
 
 
 def yt_download(video_url):
-    video_info = youtube_dl.YoutubeDL().extract_info(url=video_url, download=False)
-    filename = f"{video_info['title']}"
-    save_path = os.path.join(os.getcwd(), "CustomLevels", filename)
+    video_info = youtube_dl.YoutubeDL().extract_info(url = video_url,download=False)
+    save_path = os.path.join(os.getcwd(), "CustomLevels")
 
-    options = {
+    options={
         'format': 'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-
-        }],
-        'outtmpl': save_path
+            'preferredcodec': 'mp3',
+            'preferredquality': '192', 
+         }], 
+         'outtmpl': save_path + '/%(title)s.%(ext)s',
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
