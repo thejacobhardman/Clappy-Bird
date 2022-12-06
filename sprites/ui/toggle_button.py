3883e4818022a04254f543c70aac71880dae3a15
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-12-05 23:43:31 -0700

    attempt to abstract the toggle button

--- sprites/ui/toggle_button.py
+++ sprites/ui/toggle_button.py
@@ -5,16 +5,21 @@ import globals as g
 # Button that acts as a toggle for different options
 class ToggleButton(Button):
 
-    def __init__(self, image_file, position, text="", function=""):
-        super().__init__(image_file, position, text)
+    def __init__(self, image_file, position, display_text="", option_text="", global_variable=False, function=""):
+        super().__init__(image_file, position, display_text)
+        self.display_text = display_text
+        self.option_text = option_text
+        self.global_variable = global_variable # Needs to be a boolean not a string
         self.image_file = image_file
         self.position = position
-        self.text = text
         self.function = function
 
-    def update_text(self, image_file, position, text=("Absolute Unit Mode " + str(not g.absolute_unit_mode))):
-        super().__init__(image_file, position, text)
+    def update_text(self, image_file, position):
+        #scripts.get_updated_global_variable(self.global_variable)
+        self.global_variable = g.absolute_unit_mode # This isn't great for abstraction but it's the best I can come up with right now.
+        self.display_text = (self.option_text + " " + str(not self.global_variable))
+        super().__init__(image_file, position, self.display_text)
 
     def on_click(self):
-        self.update_text(self.image_file, self.position, text=("Absolute Unit Mode " + str(not g.absolute_unit_mode)))
+        self.update_text(self.image_file, self.position)
         scripts.execute_script(self.function)
