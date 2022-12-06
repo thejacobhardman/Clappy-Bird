e3c203178940292669d7426d047ec9e70989f7d3
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2022-12-05 22:59:54 -0700

    add updating text to toggle button

--- sprites/ui/toggle_button.py
+++ sprites/ui/toggle_button.py
@@ -1,12 +1,20 @@
 from sprites.ui.button import Button
 import scripts
+import globals as g
 
 # Button that acts as a toggle for different options
 class ToggleButton(Button):
 
     def __init__(self, image_file, position, text="", function=""):
         super().__init__(image_file, position, text)
+        self.image_file = image_file
+        self.position = position
+        self.text = text
         self.function = function
 
+    def update_text(self, image_file, position, text=("Absolute Unit Mode " + str(not g.absolute_unit_mode))):
+        super().__init__(image_file, position, text)
+
     def on_click(self):
+        self.update_text(self.image_file, self.position, text=("Absolute Unit Mode " + str(not g.absolute_unit_mode)))
         scripts.execute_script(self.function)
